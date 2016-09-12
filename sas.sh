65815fc4d5a170f2fe69624254fdcefa43f0368a
Jade Thornton <jmthornton4@gmail.com>
2016-09-11 21:03:30 -0500

    switch echo to printf

--- sas.sh
+++ sas.sh
@@ -1,83 +1,92 @@
 #!/bin/bash
 
+
+# SETTINGS
+
+notify-sound=""
+
+
+# END SETTINGS
+
+
 clear
 
-echo
-echo "A Sweet and Simple Pomodoro Timer"
-echo
+printf '%s\n'
+printf '%s\n' "A Sweet and Simple Pomodoro Timer"
+printf '%s\n'
 
 re='^[0-9]+$'
 runtime=0
 
 while true
 do
-    echo "One cycle is 135 minutes long"
+    printf '%s\n' "One cycle is 135 minutes long"
     read -p "How many cycles would you like to run? " cycles
     if ! [[ $cycles =~ $re ]]; then
-        echo "Please enter a number"
+        printf '%s\n' "Please enter a number"
     else
         cy=$(($cycles * 135))
         read -p "Confirm run timer for $cy minutes (Y/N) " yn
         case $yn in
             [Yy]* ) runtime=$cycles; break;;
-            [Nn]* ) echo;;
-            * ) echo "please answer yes or no";;
+            [Nn]* ) printf '%s\n';;
+            * ) printf '%s\n' "please answer yes or no";;
         esac
     fi
 done
 
 wd=$(pwd)
-rel_path="$wd/p.png"
+rel_path="$wd/assets/p.png"
 cycle_counter=$cycles
 
 while [ $cycle_counter -gt 0 ]
 do
 
-    echo
+    printf '%s\n'
     cycle_num=$(($cycles - $cycle_counter + 1))
-    echo "Beginning pomodoro cycle $cycle_num of $cycles"
+    printf '%s\n' "Beginning pomodoro cycle $cycle_num of $cycles"
     notify-send "Starting timer cycle" "Starting pomodoro cycle $cycle_num of $cycles" -i $rel_path -t 10000
-    echo "Type ^C at any time to exit timer"
-    echo
+    printf '%s\n' "Type ^C at any time to exit timer"
+    printf '%s\n'
 
     notify-send "Pomodoro" "Time to get to work" -i $rel_path -t 10000
 
     for a in 1 2 3
     do
-        echo "Entering pomodoro $a of 4 for 25 minutes"
-        sleep 300 && echo "20 minutes remaining"
-        sleep 300 && echo "15 minutes remaining"
-        sleep 300 && echo "10 minutes remaining"
-        sleep 300 && echo "5 minutes remaining"
-        sleep 240 && echo "1 minute remaining"
+        printf '%s\n' "Entering pomodoro $a of 4 for 25 minutes"
+        sleep 300 && printf '%s\n' "20 minutes remaining"
+        sleep 300 && printf '%s\n' "15 minutes remaining"
+        sleep 300 && printf '%s\n' "10 minutes remaining"
+        sleep 300 && printf '%s\n' "5 minutes remaining"
+        sleep 240 && printf '%s\n' "1 minute remaining"
         sleep 60 && notify-send "Break Time" "Time for a five minute break" -i $rel_path -t 10000
-        echo "End of pomodoro $a"
-        echo "Entering 5 minute break"
+        printf '%s\n' "End of pomodoro $a"
+        printf '%s\n' "Entering 5 minute break"
         sleep 300 && notify-send "Pomodoro" "Time to get back to working" -i $rel_path -t 10000
-        echo
+        printf '%s\n'
     done
-    echo "Entering pomodoro 4 of 4 for 25 minutes"
-    sleep 300 && echo "20 minutes remaining"
-    sleep 300 && echo "15 minutes remaining"
-    sleep 300 && echo "10 minutes remaining"
-    sleep 300 && echo "5 minutes remaining"
-    sleep 240 && echo "1 minute remaining"
-    echo
-    echo "Entering long break for 20 minutes"
+    printf '%s\n' "Entering pomodoro 4 of 4 for 25 minutes"
+    sleep 300 && printf '%s\n' "20 minutes remaining"
+    sleep 300 && printf '%s\n' "15 minutes remaining"
+    sleep 300 && printf '%s\n' "10 minutes remaining"
+    sleep 300 && printf '%s\n' "5 minutes remaining"
+    sleep 240 && printf '%s\n' "1 minute remaining"
+    printf '%s\n'
+    printf '%s\n' "Entering long break for 20 minutes"
     sleep 60 && notify-send "Long Break Time" "Time for a 20 minute break" -i $rel_path -t 10000
-    sleep 300 && echo "15 minutes remaining in long break"
-    sleep 300 && echo "10 minutes remaining in long break"
-    sleep 300 && echo "5 minutes remaining in long break"
-    sleep 240 && echo "1 minute remaining in long break"
+    sleep 300 && printf '%s\n' "15 minutes remaining in long break"
+    sleep 300 && printf '%s\n' "10 minutes remaining in long break"
+    sleep 300 && printf '%s\n' "5 minutes remaining in long break"
+    sleep 240 && printf '%s\n' "1 minute remaining in long break"
     sleep 60 && notify-send "End of cycle" "Cycle $cycle_num of $cycles has ended" -i $rel_path -t 10000
-    echo
+    printf '%s\n'
 
     cycle_counter=$(($cycle_counter - 1))
 
 done
 
-echo
-echo "End of timer"
-echo "Thank you for using the SAS Pomodoro Timer"
-echo
+printf '%s\n'
+printf '%s\n' "End of timer"
+printf '%s\n' "Thank you for using the SAS Pomodoro Timer"
+printf '%s\n'
 notify-send "End of Pomodoro Timer" "Please run process again for more cycles" -i $rel_path -t 17000
