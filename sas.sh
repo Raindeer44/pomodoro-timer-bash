85520acc192a806f60d99dfbc37adf218b23c9b1
Jade Thornton <jmthornton4@gmail.com>
2016-09-11 22:45:56 -0500

    timing bug fix

--- sas.sh
+++ sas.sh
@@ -4,7 +4,7 @@
 # SETTINGS
 
 notify_sound="pluck.wav" # Change to empty string for no sound
-# to preview sound, use `aplay assets/soundfile.wav` in terminal
+# to preview sound, use `aplay -q assets/soundfile.wav` in terminal
 #pomodoro_length="25" # not implemented yet
 #short_break_length="5" # not implemented yet
 #long_break_length="20" # not implemented yet
@@ -54,7 +54,7 @@ do
     printf '%s\n'
 
     notify-send "Pomodoro" "Time to get to work" -i $icon -t 10000
-    aplay $sound
+    aplay -q $sound
 
     for a in 1 2 3
     do
@@ -65,11 +65,11 @@ do
         sleep 300 && printf '%s\n' "5 minutes remaining"
         sleep 240 && printf '%s\n' "1 minute remaining"
         sleep 60 && notify-send "Break Time" "Time for a five minute break" -i $icon -t 10000
-        aplay $sound
+        aplay -q $sound
         printf '%s\n' "End of pomodoro $a"
         printf '%s\n' "Entering 5 minute break"
         sleep 300 && notify-send "Pomodoro" "Time to get back to working" -i $icon -t 10000
-        aplay $sound
+        aplay -q $sound
         printf '%s\n'
     done
     printf '%s\n' "Entering pomodoro 4 of 4 for 25 minutes"
@@ -81,7 +81,7 @@ do
     printf '%s\n'
     printf '%s\n' "Entering long break for 20 minutes"
     sleep 60 && notify-send "Long Break Time" "Time for a 20 minute break" -i $icon -t 10000
-    aplay $sound
+    aplay -q $sound
     sleep 300 && printf '%s\n' "15 minutes remaining in long break"
     sleep 300 && printf '%s\n' "10 minutes remaining in long break"
     sleep 300 && printf '%s\n' "5 minutes remaining in long break"
@@ -99,4 +99,4 @@ printf '%s\n' "End of timer"
 printf '%s\n' "Thank you for using the SAS Pomodoro Timer"
 printf '%s\n'
 notify-send "End of Pomodoro Timer" "Please run process again for more cycles" -i $icon -t 17000
-aplay $sound
+aplay -q $sound
